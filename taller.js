297eff7521832c4e899ea1ce713ebd8ce6c198d4
SaraPerilla <sperillac@unbosque.edu.co>
2026-04-16 08:31:09 -0500

    modificaciones

--- taller.js
+++ taller.js
@@ -24,3 +24,13 @@ let salarioTotal = salario + comisiones + horasExtras ;
 let IBC = salarioTotal * porcentajeIBC ;
 let salud = IBC * porcentajeSalud ;
 let pension = IBC * porcentajePension 
+
+if ( edad < 18 ) {
+    console.log ("No se puede calcular");
+} else if ( edad >= 18 && edad < 25 ) {
+     console.log ("Usuario beneficiario por cotizante");
+} else if ( edad >= 25 && edad < 60 ) {
+    console.log ("Se calcularà la pensiòn");
+} else { 
+    console.log ("Podrà continuar con el siguiente paso del proceso");
+}
