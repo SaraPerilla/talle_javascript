a3f89557ed2f04eabff8f9451c194656a07ecac6
SaraPerilla <sperillac@unbosque.edu.co>
2026-04-16 08:50:53 -0500

    update taller

--- taller.js
+++ taller.js
@@ -7,6 +7,16 @@ let comisiones = 0 ;
 let horasExtras = 0 ;
 let nivelRiesgo = "" ;
 
+if ( edad < 18 ) {
+    console.log ("No se puede calcular");
+} else if ( edad >= 18 && edad < 25 ) {
+     console.log ("Usuario beneficiario por cotizante");
+} else if ( edad >= 25 && edad < 60 ) {
+    console.log ("Se calcularà la pensiòn");
+} else { 
+    console.log ("Podrà continuar con el siguiente paso del proceso");
+}
+
 const salarioMinimoLegalV = 1750905 ;
 const salarioMinimoIntegralV = 22761765 ;
 const subsidioTransporte = 249095 ;
@@ -20,17 +30,11 @@ const porcentajeIBC = 0.7 ;
 const porcentajeSalud = 0.04 ;
 const porcentajePension = 0.04 ;
 
-let salarioTotal = salario + comisiones + horasExtras ;
-let IBC = salarioTotal * porcentajeIBC ;
-let salud = IBC * porcentajeSalud ;
-let pension = IBC * porcentajePension 
-
-if ( edad < 18 ) {
-    console.log ("No se puede calcular");
-} else if ( edad >= 18 && edad < 25 ) {
-     console.log ("Usuario beneficiario por cotizante");
-} else if ( edad >= 25 && edad < 60 ) {
-    console.log ("Se calcularà la pensiòn");
-} else { 
-    console.log ("Podrà continuar con el siguiente paso del proceso");
+function calcularPorcentaje ( base, porcentaje ) {
+    return base * porcentaje
 }
+
+let salarioTotal = salario + comisiones + horasExtras ;
+let IBC = calcularPorcentaje ( salarioTotal, porcentajeIBC );
+let salud = calcularPorcentaje ( IBC, porcentajeSalud);
+let pension = calcularPorcentaje ( IBC, porcentajePension);
